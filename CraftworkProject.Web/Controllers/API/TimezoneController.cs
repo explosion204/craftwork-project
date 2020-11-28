31af47c05e62f9bbf571a17f8831d17fa29ad9cd
explosion204 <dzmitriy20magic@gmail.com>
2020-11-28 13:26:00 +0300

    added local time representation for user

---
+++ CraftworkProject.Web/Controllers/API/TimezoneController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CraftworkProject.Web.Controllers.API
+{
+    public class TimezoneController : Controller
+    {
+        [HttpPost]
+        public IActionResult Index(string timeZoneOffset)
+        {
+            HttpContext.Session.SetString("timeZoneOffset", timeZoneOffset);
+            return Json(new {success = true});
+        }
+    }
+}
